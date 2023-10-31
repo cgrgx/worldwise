e2b82a0da82d2ca4e2a8cd4a92a4c9fe0d14200e
Chiran Gurung <grgchiran08@yahoo.com>
2023-10-31 05:20:51 -0400

    setting a Query String

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
-}
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>
+        PositionL {lat}, {lng}
+      </p>
 
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change Pos
+      </button>
+    </div>
+  );
+}
 export default Map;
