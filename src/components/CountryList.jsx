6627b10cba4b8409981a6f0f6b3ca003632ae60a
Chiran Gurung <grgchiran08@yahoo.com>
2023-11-03 05:34:52 -0400

    consuming city context

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CityContext";
 
-function CityList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -16,7 +19,6 @@ function CityList({ cities, isLoading }) {
       return [...arr, { country: city.country, emoji: city.emoji }];
     } else return arr;
   }, []);
-  console.log(countries);
 
   return (
     <ul className={styles.countryList}>
@@ -27,4 +29,4 @@ function CityList({ cities, isLoading }) {
   );
 }
 
-export default CityList;
+export default CountryList;
